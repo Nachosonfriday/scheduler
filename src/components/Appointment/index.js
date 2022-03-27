4cc5851d5dc60b5789956459dc2b1da88fc486a2
Manuel Zuniga <zuniga@ualberta.ca>
2022-03-27 14:30:23 -0600

    add create form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,16 +3,44 @@ import React from "react";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form";
 
-import 'components/Appointment/styles.scss';
+import useVisualMode from "hooks/useVisualMode";
+
+import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
+  console.log("test", props)
 
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+    console.log("mode", mode)
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show student={props.interview.student} interview={props.interview.interviewer} />
-      : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+        //optional chaining null or undefined check
+        interviewer={(props.interview?.interviewer)} 
+        interviewers = {[]}
+        onCancel = {back}
+        
+      />
+      )}
     </article>
   );
 }
+
+
